10501d30045abf69de35c806cdb105452fb467f7
24stardust <24laststardust@gmail.com>
2023-07-10 14:49:44 +0900

    third commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
 
-    // 메뉴 화살표 돌아가는 거 찾아서 넣기
-    // $('.mb-menu>li').click(function(){
-    // })
-
 
     $('.mobile-burger').click(()=>{
         $('.mb-nav').slideToggle(200);
@@ -27,21 +23,4 @@ $(document).ready(function(){
         console.log(count)
         mb_submenu_height[index]= 55*count+20;
     })
-
-    // let mb_li = $('.mb-menu > li')
-    // $.each(mb_mainmenu, function(index){
-    //     $(this).click(function(e){
-    //         e.preventDefault();
-    //         $(this).toggleClass('mb-mainmenu-open')
-    //         let active = $(this).hasClass('mb-mainmenu-open')
-    //         if(active){
-    //             let temp = mb_submenu_height[index]
-    //             mb_li.eq(index).height(temp+55)
-    //             // mb_li.eq(index).siblings().height(70)
-    //         }else {
-    //             mb_li.eq(index).height(70)
-    //         }
-    //     })
-    // })
-
 })
